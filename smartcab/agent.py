51354a20cb259561787e43f112920999a917be5e
James Nguyen <jklnguyen@ucdavis.edu>
2017-04-24 08:38:27 -0700

    feat:Answered problem 4

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -95,7 +95,7 @@ class LearningAgent(Agent):
         # Set the agent state and default action
         self.state = state
         self.next_waypoint = self.planner.next_waypoint()
-        action = None
+        action = random.choice(Environment.valid_actions)
 
         ###########
         ## TO DO ##
@@ -175,7 +175,7 @@ def run():
     # Flags:
     #   tolerance  - epsilon tolerance before beginning testing, default is 0.05
     #   n_test     - discrete number of testing trials to perform, default is 0
-    sim.run(n_test = 10)
+    sim.run(n_test = 5)
 
 
 if __name__ == '__main__':
